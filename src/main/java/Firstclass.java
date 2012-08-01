d1360e32a5592f14e45222e4b9a7208118e51ea7
kphani <pkaliginadi@gmail.com>
2012-08-01 21:04:01 +0100

    first commit

---
+++ src/main/java/Firstclass.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kphani
+ * Date: 31/7/12
+ * Time: 9:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class Firstclass {
+
+    WebDriver driver =new FirefoxDriver();
+
+    @Test
+    public void test(){
+    driver.get("http://www.google.com");
+    driver.findElement(By.id("gbqfq")).sendKeys("phani");
+
+
+
+
+
+   }
+
+
+
+
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kphani
+ * Date: 31/7/12
+ * Time: 9:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+
+    public static void main(String args[]){
+
+        System.out.println("this is phani");
+    }
+}
